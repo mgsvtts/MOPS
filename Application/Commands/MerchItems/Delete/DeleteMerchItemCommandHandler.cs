a75873faf0e39b459f1c73edbf8143f064309698
mgsvtts <mgsvtts1@gmail.com>
2023-11-06 00:07:04 +0300

    delete image during merch item deletion

--- Application/Commands/MerchItems/Delete/DeleteMerchItemCommandHandler.cs
+++ Application/Commands/MerchItems/Delete/DeleteMerchItemCommandHandler.cs
@@ -6,10 +6,12 @@ namespace Application.Commands.MerchItems.Delete;
 public class DeleteMerchItemCommandHandler : IRequestHandler<DeleteMerchItemCommand>
 {
     private readonly IMerchItemRepository _merchRepository;
+    private readonly IImageRepository _imageRepository;
 
-    public DeleteMerchItemCommandHandler(IMerchItemRepository merchRepository)
+    public DeleteMerchItemCommandHandler(IMerchItemRepository merchRepository, IImageRepository imageRepository)
     {
         _merchRepository = merchRepository;
+        _imageRepository = imageRepository;
     }
 
     public async Task Handle(DeleteMerchItemCommand request, CancellationToken cancellationToken)
@@ -17,6 +19,7 @@ public class DeleteMerchItemCommandHandler : IRequestHandler<DeleteMerchItemComm
         var item = await _merchRepository.GetByIdAsync(request.Id)
                    ?? throw new InvalidOperationException($"Cannot find merch item with id {request.Id.Identity}");
 
+        await Task.WhenAll(item.Images.Select(_imageRepository.DeleteAsync));
         await _merchRepository.DeleteAsync(item);
     }
 }
